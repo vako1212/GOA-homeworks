7df34c3d5d26af6ee8a74b65064675d69572c197
vako1212 <vakobalabanashvili03@gmail.com>
2024-05-11 18:57:37 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,33 @@
+name = "vako"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "vako" არის ცვლადისთვის მნიშვნელობა
+
+surname = "balabanashvili"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "vako"  #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მთელი რიცხვი
+height = 165 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = true  #True ან False
+is_ugly = False   #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+# print(name + " " + surname)
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print (name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age)) 
