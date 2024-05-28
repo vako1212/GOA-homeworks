826a4b60e6e389bf8245c6a35146d458459ad45a
vako1212 <vakobalabanashvili03@gmail.com>
2024-05-28 18:45:57 +0400

    second commit

--- Level 00/Lesson 0.py
+++ Level 00/Lesson 0.py
@@ -3,7 +3,7 @@ from turtle import *
 #we want to paint a house
 
 #step 1: draw a square
-speed(15)
+speed(30)
 width(7)
 color("purple")
 forward(200)
---
+++ Level 05/homework2.py
@@ -0,0 +1,24 @@
+num1 = 4
+num2 = 2
+print (num1 + num2)
+print (num1 // num2)
+print (num1 * num2)
+print (num1 - num2)
+num3 = 4
+num4 = 6
+print(num3 + num4)
+print(num3 // num4)
+print(num3 * num4)
+print(num3 - num4)
+num5 = 10
+num6 = 12
+print(num6 + num5)
+print(num6 // num5)
+print(num6 * num5)
+print(num6 - num5)
+num7 = 14
+num8 = 16
+print(num7 + num8)
+print(num7 // num8)
+print(num7 * num8)
+print(num7 - num8)
